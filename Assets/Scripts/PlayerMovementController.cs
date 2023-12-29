d8130652de6ff2fcf4adcc7173acffbced2ae89d
Zehra Ünsaçarer <117662187+ZehraUnsacarer@users.noreply.github.com>
2023-12-29 19:41:15 +0300

    Add files via upload

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed; 
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Movement();
+     
+    }
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * moveSpeed;
+    }
+}
